7a1b72c626a0c328f4e6a9d733d2411d62b10bbb
vishalbharath <vishalbharathonly@gmail.com>
2025-05-05 12:13:33 +0530

    changes made

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
-const path = require('path')
- 
+const path = require('path');
+
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
@@ -23,4 +23,5 @@ module.exports = {
       },
     ],
   },
-}
+  telemetry: false,  // Disable telemetry
+};
