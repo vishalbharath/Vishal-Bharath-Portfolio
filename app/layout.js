f00a9b495e39f335a7e0d1f3ef55a7f8a6bd4518
vishalbharath <vishalbharathonly@gmail.com>
2025-05-05 11:44:59 +0530

    update done

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,6 @@ import ScrollToTop from "./components/helper/scroll-to-top";
 import Navbar from "./components/navbar";
 import "./css/card.scss";
 import "./css/globals.scss";
-import Head from "next/head"; // ✅ Import Head properly
 
 export const metadata = {
   title: "Portfolio of Vishal Bharath - Software Developer",
@@ -17,14 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        {/* ✅ Google Fonts link correctly placed */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <body style={{ fontFamily: "Inter, sans-serif" }}>
+      <body>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
           <Navbar />
@@ -32,8 +24,8 @@ export default function RootLayout({ children }) {
           <ScrollToTop />
         </main>
         <Footer />
-        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
       </body>
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -9,10 +9,10 @@ import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
 async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
+  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`);
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data')
+    throw new Error('Failed to fetch data');
   }
 
   const data = await res.json();
@@ -20,13 +20,13 @@ async function getData() {
   const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
 
   return filtered;
-};
+}
 
 export default async function Home() {
   const blogs = await getData();
 
   return (
-    <div suppressHydrationWarning >
+    <div suppressHydrationWarning>
       <HeroSection />
       <AboutSection />
       <Experience />
@@ -36,5 +36,5 @@ export default async function Home() {
       <Blog blogs={blogs} />
       <ContactSection />
     </div>
-  )
-};
+  );
+}
